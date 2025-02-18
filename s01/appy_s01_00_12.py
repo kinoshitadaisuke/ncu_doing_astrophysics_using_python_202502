53001630dd92730d244d2c27aad63d753bfe6f6a
Daisuke <mhdf23888@gmail.com>
2025-02-18 23:00:12 +0800

    adding "s01/appy_s01_00_12.py" on 18/Feb/2025

---
+++ s01/appy_s01_00_12.py
@@ -0,0 +1,15 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/02/18 22:37:49 (CST) daisuke>
+#
+
+# assignment of values to variables
+c  = 299792458
+au = 149597870700
+pc = 3.08567758 * 10**16
+
+# fancy formatting using formatted string literals
+print (f'c    = {c:g} [m/s]')
+print (f'1 au = {au:g} [m]')
+print (f'1 pc = {pc:g} [m]')
