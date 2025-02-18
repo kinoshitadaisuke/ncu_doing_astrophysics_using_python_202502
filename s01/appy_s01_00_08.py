fe929da86c4b14df0f536c182bd914453b2b840a
Daisuke <mhdf23888@gmail.com>
2025-02-18 23:00:06 +0800

    adding "s01/appy_s01_00_08.py" on 18/Feb/2025

---
+++ s01/appy_s01_00_08.py
@@ -0,0 +1,11 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/02/18 22:37:31 (CST) daisuke>
+#
+
+# assignment of a value to a variable
+a = 3 + 4j
+
+# printing the value of a variable using built-in function "print ()"
+print ('a = %f + %fi' % (a.real, a.imag))
