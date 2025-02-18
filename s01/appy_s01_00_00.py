327de12791d5c9e89f98827c1b20c1d49a0b857b
Daisuke <mhdf23888@gmail.com>
2025-02-18 22:59:52 +0800

    adding "s01/appy_s01_00_00.py" on 18/Feb/2025

---
+++ s01/appy_s01_00_00.py
@@ -0,0 +1,8 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/02/18 22:36:50 (CST) daisuke>
+#
+
+# printing a sentence using built-in function "print ()"
+print ('I love astrophysics! I love Python!')
