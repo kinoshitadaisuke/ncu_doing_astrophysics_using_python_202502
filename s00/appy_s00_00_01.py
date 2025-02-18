acd706c6d80e7c0919235a2adb59fac3bead28af
Daisuke <mhdf23888@gmail.com>
2025-02-18 21:44:29 +0800

    adding "s00/appy_s00_00_01.py" on 18/Feb/2025

---
+++ s00/appy_s00_00_01.py
@@ -0,0 +1,13 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/02/18 21:40:37 (CST) daisuke>
+#
+
+# calculation
+a = 3
+b = 5
+c = a + b
+
+# printing result of calculation
+print (f'{a} + {b} = {c}')
