29975c5e3173a88a59ea4d6d30a89cb256f720d0
Daisuke <mhdf23888@gmail.com>
2025-02-18 23:00:09 +0800

    adding "s01/appy_s01_00_10.py" on 18/Feb/2025

---
+++ s01/appy_s01_00_10.py
@@ -0,0 +1,15 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/02/18 22:37:40 (CST) daisuke>
+#
+
+# assignment of values to variables by multiple assignment
+a, b = 1.2, 3.4
+
+# calculation
+c = a * b
+
+# printing values of variables using built-in function "print ()"
+# and printf-style string formatting
+print ('%f * %f = %f' % (a, b, c) )
