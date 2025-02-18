8e6d7af90033a214667673fc709a9a30efcba527
Daisuke <mhdf23888@gmail.com>
2025-02-18 22:59:54 +0800

    adding "s01/appy_s01_00_01.py" on 18/Feb/2025

---
+++ s01/appy_s01_00_01.py
@@ -0,0 +1,11 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/02/18 22:36:57 (CST) daisuke>
+#
+
+# assignment of a value to a variable
+sentence = 'Astronomy is a fun!'
+
+# printing the value of a variable using built-in function "print ()"
+print (sentence)
