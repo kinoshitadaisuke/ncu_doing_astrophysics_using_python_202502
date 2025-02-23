110ecd95fe526e07d7683976fc3a33adec42eb2c
Daisuke <mhdf23888@gmail.com>
2025-02-23 16:51:25 +0800

    adding "s02/appy_s02_01_02.py" on 23/Feb/2025

---
+++ s02/appy_s02_01_02.py
@@ -0,0 +1,20 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/09/17 13:16:27 (UT+8) daisuke>
+#
+
+# importing sys module
+import sys
+
+# receiving command-line arguments
+args = sys.argv
+
+# number of command-line arguments
+n_args = len (args)
+
+# printing number of command-line arguments
+print (f'number of command-line arguments = {n_args}')
+
+# printing command-line arguments
+print (f'arguments = {args}')
