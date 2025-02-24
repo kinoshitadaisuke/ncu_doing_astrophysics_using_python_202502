97b2c8f63edf003c5f48e2e278e8a0b96ca6b09b
Daisuke <mhdf23888@gmail.com>
2025-02-24 21:43:16 +0800

    adding "s02/appy_s02_07_01.py" on 24/Feb/2025

--- s02/appy_s02_07_01.py
+++ s02/appy_s02_07_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/09/17 17:08:18 (UT+8) daisuke>
+# Time-stamp: <2025/02/24 21:26:06 (CST) daisuke>
 #
 
 # importing multiprocessing module
