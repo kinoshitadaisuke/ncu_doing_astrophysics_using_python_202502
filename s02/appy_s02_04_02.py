a47320056a7fb18595e86e64e1f5cff8b936a108
Daisuke <mhdf23888@gmail.com>
2025-02-24 21:28:14 +0800

    adding "s02/appy_s02_04_02.py" on 24/Feb/2025

--- s02/appy_s02_04_02.py
+++ s02/appy_s02_04_02.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/09/17 16:23:16 (UT+8) daisuke>
+# Time-stamp: <2025/02/24 21:22:18 (CST) daisuke>
 #
 
 # importing pathlib module
