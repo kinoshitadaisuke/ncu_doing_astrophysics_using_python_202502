95a8cd4d4e9c75f3f4a9d05b263154d06f558bc8
Daisuke <mhdf23888@gmail.com>
2025-02-24 21:47:29 +0800

    adding "s02/appy_s02_13_04.py" on 24/Feb/2025

--- s02/appy_s02_13_04.py
+++ s02/appy_s02_13_04.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/09/17 17:37:44 (UT+8) daisuke>
+# Time-stamp: <2025/02/24 21:35:44 (CST) daisuke>
 #
 
 # importing decimal module
