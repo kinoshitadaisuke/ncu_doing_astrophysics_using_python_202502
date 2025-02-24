c809dd8a810dab6c54a718f9a3b765abb3f61fe0
Daisuke <mhdf23888@gmail.com>
2025-02-24 21:28:08 +0800

    adding "s02/appy_s02_03_01.py" on 24/Feb/2025

--- s02/appy_s02_03_01.py
+++ s02/appy_s02_03_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/09/17 16:10:24 (UT+8) daisuke>
+# Time-stamp: <2025/02/24 21:20:59 (CST) daisuke>
 #
 
 # input file name
