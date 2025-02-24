b47c69ebfd69883487e3bbef62dcd936108aa6e2
Daisuke <mhdf23888@gmail.com>
2025-02-24 21:45:13 +0800

    adding "s02/appy_s02_11_00.py" on 24/Feb/2025

--- s02/appy_s02_11_00.py
+++ s02/appy_s02_11_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/09/17 17:28:40 (UT+8) daisuke>
+# Time-stamp: <2025/02/24 21:33:51 (CST) daisuke>
 #
 
 # importing random module
