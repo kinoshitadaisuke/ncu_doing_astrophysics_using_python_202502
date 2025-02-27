a7a3e46244ab8b78027983f318523507bfd1a8d9
Daisuke <mhdf23888@gmail.com>
2025-02-27 11:33:37 +0800

    adding "s03/appy_s03_09_02.py" on 27/Feb/2025

---
+++ s03/appy_s03_09_02.py
@@ -0,0 +1,17 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/09/24 21:02:09 (UT+8) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# explicitly specify PCG64 for random number generator
+rng = numpy.random.Generator (numpy.random.PCG64DXSM ())
+
+# generating a random number of uniform distribution between 0 and 1
+array_x = rng.random ()
+
+# printing generated random numbers
+print (f'{array_x}')
