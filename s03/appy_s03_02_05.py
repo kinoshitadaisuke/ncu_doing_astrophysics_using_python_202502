c5fbbf59bfba6762b531f1ba9e92fbfda7981e82
Daisuke <mhdf23888@gmail.com>
2025-03-02 13:44:14 +0800

    adding "s03/appy_s03_02_05.py" on  2/Mar/2025

--- s03/appy_s03_02_05.py
+++ s03/appy_s03_02_05.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/09/24 13:55:49 (UT+8) daisuke>
+# Time-stamp: <2025/03/02 12:31:04 (CST) daisuke>
 #
 
 # importing numpy module
