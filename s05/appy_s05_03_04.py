c217cc54e1fe0eba05fabb8763f1e04b8db121d5
Daisuke <mhdf23888@gmail.com>
2025-03-12 18:08:38 +0800

    adding "s05/appy_s05_03_04.py" on 12/Mar/2025

---
+++ s05/appy_s05_03_04.py
@@ -0,0 +1,24 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/10/07 15:20:06 (UT+8) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# importing scipy module
+import scipy
+import scipy.linalg
+
+# matrix A
+A = numpy.array ( [ [3.0, 1.0], [2.0, 2.0] ] )
+
+# printing matrix A
+print (f'matrix A:\n{A}')
+
+# the other way to get eigenvalues of matrix A
+eigenvalues = scipy.linalg.eigvals (A)
+
+# printing eigenvalues of matrix A
+print (f'eigenvalues of matrix A:\n{eigenvalues}')
