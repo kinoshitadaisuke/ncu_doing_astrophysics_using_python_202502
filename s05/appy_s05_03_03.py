106b51c65e34e9823213d4387701ed1f8d247997
Daisuke <mhdf23888@gmail.com>
2025-03-12 18:08:36 +0800

    adding "s05/appy_s05_03_03.py" on 12/Mar/2025

---
+++ s05/appy_s05_03_03.py
@@ -0,0 +1,25 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/10/07 15:19:31 (UT+8) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# importing scipy module
+import scipy
+import scipy.linalg
+
+# matrix A
+A = numpy.array ( [ [3.0, 1.0], [2.0, 2.0] ] )
+
+# printing matrix A
+print (f'matrix A:\n{A}')
+
+# eigenvalues and eigenvectors of matrix A
+eigenvalvec = scipy.linalg.eig (A)
+
+# printing eigenvalues and eigenvectors of matrix A
+print (f'eigenvalues of matrix A:\n{eigenvalvec[0]}')
+print (f'eigenvectors of matrix A:\n{eigenvalvec[1]}')
