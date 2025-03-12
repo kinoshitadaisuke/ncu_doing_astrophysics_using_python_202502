06f612ad85625c2218e83c7ecf60f148c7c12320
Daisuke <mhdf23888@gmail.com>
2025-03-12 18:09:05 +0800

    adding "s05/appy_s05_05_05.py" on 12/Mar/2025

---
+++ s05/appy_s05_05_05.py
@@ -0,0 +1,33 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/10/07 16:07:54 (UT+8) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# importing scipy module
+import scipy
+import scipy.integrate
+
+# function of f(x) = cos (x)
+def curve (x):
+    # curve
+    y = numpy.cos (x)
+    # returning a value
+    return (y)
+
+# data points
+data_x = numpy.linspace (0.0, numpy.pi / 2.0, 1001)
+data_y = curve (data_x)
+
+# printing data_x and data_y
+print (f'data_x = {data_x}')
+print (f'data_y = {data_y}')
+
+# numerical integration of given data points by trapezoidal rule
+I = scipy.integrate.trapezoid (data_y, x=data_x)
+
+# printing result of numerical integration
+print (f'I = {I}')
