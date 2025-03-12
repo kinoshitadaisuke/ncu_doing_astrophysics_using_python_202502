79b937f175a354601742fd5a842643ed0ba1e892
Daisuke <mhdf23888@gmail.com>
2025-03-12 18:07:47 +0800

    adding "s05/appy_s05_00_00.py" on 12/Mar/2025

---
+++ s05/appy_s05_00_00.py
@@ -0,0 +1,14 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/10/06 20:01:44 (UT+8) daisuke>
+#
+
+# importing scipy module
+import scipy.constants
+
+# value of pi
+pi = scipy.constants.pi
+
+# printing value of pi
+print (f'pi = {pi}')
