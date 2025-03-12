5d98c5e5a239b58ed489436250a95d70da5f8b26
Daisuke <mhdf23888@gmail.com>
2025-03-12 18:08:31 +0800

    adding "s05/appy_s05_03_00.py" on 12/Mar/2025

---
+++ s05/appy_s05_03_00.py
@@ -0,0 +1,24 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/10/07 15:12:42 (UT+8) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# importing scipy module
+import scipy
+import scipy.linalg
+
+# matrix A
+A = numpy.array ( [ [4.0, 11.0], [2.0, 5.0] ] )
+
+# printing matrix A
+print (f'matrix A:\n{A}')
+
+# determinant of matrix A
+A_det = scipy.linalg.det (A)
+
+# printing the determinant of matrix A
+print (f'determinant of matrix A = {A_det}')
