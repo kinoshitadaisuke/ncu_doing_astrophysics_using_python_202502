f3201b2873a5d4a1a55baae8357a765ace27a554
Daisuke <mhdf23888@gmail.com>
2025-03-12 18:08:56 +0800

    adding "s05/appy_s05_05_00.py" on 12/Mar/2025

---
+++ s05/appy_s05_05_00.py
@@ -0,0 +1,29 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/10/07 16:01:40 (UT+8) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# importing scipy module
+import scipy
+import scipy.integrate
+
+# function of a curve
+def curve (x):
+    # curve
+    y = numpy.sin (x)
+    # returning a value
+    return (y)
+
+# range of integration
+x0 = 0.0
+x1 = numpy.pi
+
+# numerical integration
+result = scipy.integrate.quad (curve, x0, x1)
+
+# printing result of numerical integration
+print (f'integ. of sin (x) from 0.0 to pi = {result[0]} +/- {result[1]}')
