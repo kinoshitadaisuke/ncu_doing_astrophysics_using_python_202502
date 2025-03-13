2f872623b66f77426daa39312961d2daad867d1d
Daisuke <mhdf23888@gmail.com>
2025-03-13 20:39:59 +0800

    adding "s05/appy_s05_00_08.py" on 13/Mar/2025

--- s05/appy_s05_00_08.py
+++ s05/appy_s05_00_08.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/10/06 20:03:12 (UT+8) daisuke>
+# Time-stamp: <2025/03/13 20:10:34 (UT+08:00) daisuke>
 #
 
 # importing scipy module
