2c8979c4c69855cd44a748bb80405ef0de391468
Daisuke <mhdf23888@gmail.com>
2025-03-13 20:40:03 +0800

    adding "s05/appy_s05_01_00.py" on 13/Mar/2025

--- s05/appy_s05_01_00.py
+++ s05/appy_s05_01_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/10/07 14:53:18 (UT+8) daisuke>
+# Time-stamp: <2025/03/13 20:11:05 (UT+08:00) daisuke>
 #
 
 # importing argparse module
