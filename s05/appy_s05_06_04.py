941c35d5f8985156604616273a08cf7680f23780
Daisuke <mhdf23888@gmail.com>
2025-03-13 20:41:13 +0800

    adding "s05/appy_s05_06_04.py" on 13/Mar/2025

--- s05/appy_s05_06_04.py
+++ s05/appy_s05_06_04.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/10/07 16:17:10 (UT+8) daisuke>
+# Time-stamp: <2025/03/13 20:28:34 (UT+08:00) daisuke>
 #
 
 # importing subprocess module
