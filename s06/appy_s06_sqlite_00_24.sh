06eec128701668993e860d695b071eadfc42446c
Daisuke <mhdf23888@gmail.com>
2025-03-24 19:38:32 +0800

    adding "s06/appy_s06_sqlite_00_24.sh" on 24/Mar/2025

--- s06/appy_s06_sqlite_00_24.sh
+++ s06/appy_s06_sqlite_00_24.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2025/03/20 20:48:08 (UT+08:00) daisuke>
+# Time-stamp: <2025/03/24 19:23:24 (UT+08:00) daisuke>
 #
 
-sqlite3 dwarf_planet.db "create table dwarfplanet (name text primary key, \
-a real, e real, i real, perihelion real, aphelion real, P real, H real);"
+# SQL query
+sqlite3 dwarf_planet.db \
+	"create table dwarfplanet (name text primary key, \
+	a real, e real, i real, perihelion real, aphelion real, P real, H real);"
