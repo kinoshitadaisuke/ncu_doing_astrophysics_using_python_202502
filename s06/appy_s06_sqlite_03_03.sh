82d3343280f2e9fb887473e021f404cba8a0f48c
Daisuke <mhdf23888@gmail.com>
2025-03-24 19:39:16 +0800

    adding "s06/appy_s06_sqlite_03_03.sh" on 24/Mar/2025

--- s06/appy_s06_sqlite_03_03.sh
+++ s06/appy_s06_sqlite_03_03.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2025/03/20 23:16:35 (UT+08:00) daisuke>
+# Time-stamp: <2025/03/24 19:32:01 (UT+08:00) daisuke>
 #
 
 # SQL query
-sqlite3 --header --column hip.db "select hip, ra_hms, dec_dms, vmag, \
-bv, parallax, sptype from hip where parallax > 300 order by parallax desc;"
+sqlite3 --header --column hip.db \
+	"select hip, ra_hms, dec_dms, vmag, bv, parallax, sptype from hip \
+	where parallax > 300 order by parallax desc;"
