60c8cb6124621883005e13239ec4e16f0589bc0c
Daisuke <mhdf23888@gmail.com>
2025-03-24 19:39:07 +0800

    adding "s06/appy_s06_sqlite_02_03.sh" on 24/Mar/2025

--- s06/appy_s06_sqlite_02_03.sh
+++ s06/appy_s06_sqlite_02_03.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2025/03/20 22:43:50 (UT+08:00) daisuke>
+# Time-stamp: <2025/03/24 19:30:35 (UT+08:00) daisuke>
 #
 
 # SQL query
-sqlite3 --header --column bsc5.db "select hr, name, ra_str, dec_str, \
-vmag, bv, parallax from bsc where parallax >= 0.2 order by parallax desc;"
+sqlite3 --header --column bsc5.db \
+	"select hr, name, ra_str, dec_str, vmag, bv, parallax from bsc \
+	where parallax >= 0.2 order by parallax desc;"
