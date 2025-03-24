c130bff3183b1372cf03e9ec846be150df1bbd16
Daisuke <mhdf23888@gmail.com>
2025-03-24 19:38:53 +0800

    adding "s06/appy_s06_sqlite_00_39.sh" on 24/Mar/2025

--- s06/appy_s06_sqlite_00_39.sh
+++ s06/appy_s06_sqlite_00_39.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2025/03/20 21:08:13 (UT+08:00) daisuke>
+# Time-stamp: <2025/03/24 19:27:54 (UT+08:00) daisuke>
 #
 
 # exporting database into a CSV file
-sqlite3 dwarf_planet3.db ".header on" ".mode csv" ".once new.csv" \
-"select * from dwarfplanet;"
+sqlite3 dwarf_planet3.db \
+	".header on" \
+	".mode csv" \
+	".once new.csv" \
+	"select * from dwarfplanet;"
