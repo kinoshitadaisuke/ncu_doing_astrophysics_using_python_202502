6b50df219b345a1c30d8c5dd97fea0fb435f3260
Daisuke <mhdf23888@gmail.com>
2025-03-24 19:38:35 +0800

    adding "s06/appy_s06_sqlite_00_26.sh" on 24/Mar/2025

--- s06/appy_s06_sqlite_00_26.sh
+++ s06/appy_s06_sqlite_00_26.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2025/03/20 20:51:25 (UT+08:00) daisuke>
+# Time-stamp: <2025/03/24 19:23:50 (UT+08:00) daisuke>
 #
 
+# printing usage of ".import" command
 sqlite3 "" ".help .import"
