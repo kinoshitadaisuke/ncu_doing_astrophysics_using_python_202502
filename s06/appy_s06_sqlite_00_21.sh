b35fc8110fc514dd857cdf2f279a00bb5f2dc8a9
Daisuke <mhdf23888@gmail.com>
2025-03-24 19:38:26 +0800

    adding "s06/appy_s06_sqlite_00_21.sh" on 24/Mar/2025

--- s06/appy_s06_sqlite_00_21.sh
+++ s06/appy_s06_sqlite_00_21.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2025/03/20 20:40:53 (UT+08:00) daisuke>
+# Time-stamp: <2025/03/24 19:22:08 (UT+08:00) daisuke>
 #
 
-sqlite3 -header -column planet0.db "select name,mass,diameter,satellite, \
-ring,magnetic_field from planet where satellite >= 10;"
+# SQL query
+sqlite3 -header -column planet0.db \
+	"select name,mass,diameter,satellite,ring,magnetic_field from planet \
+	where satellite >= 10;"
