06a42dbb99a0959e8258a64526840236713728c5
Daisuke <mhdf23888@gmail.com>
2025-03-24 19:37:50 +0800

    adding "s06/appy_s06_sqlite_00_00.sh" on 24/Mar/2025

--- s06/appy_s06_sqlite_00_00.sh
+++ s06/appy_s06_sqlite_00_00.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2025/03/20 20:04:08 (UT+08:00) daisuke>
+# Time-stamp: <2025/03/24 19:33:41 (UT+08:00) daisuke>
 #
 
-sqlite3 planet0.db "create table planet (name text primary key, \
-mass real, diameter real, rotation_period real, orbital_period real, \
-mean_temperature real, satellite integer, ring text, magnetic_field text);"
+# making a table
+sqlite3 planet0.db \
+	"create table planet (name text primary key, \
+	mass real, diameter real, rotation_period real, orbital_period real, \
+	mean_temperature real, satellite integer, ring text, \
+	magnetic_field text);"
