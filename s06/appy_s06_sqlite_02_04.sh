60171d7c2463964d4879dfd09da8bfafc3052fa7
Daisuke <mhdf23888@gmail.com>
2025-03-24 19:39:09 +0800

    adding "s06/appy_s06_sqlite_02_04.sh" on 24/Mar/2025

--- s06/appy_s06_sqlite_02_04.sh
+++ s06/appy_s06_sqlite_02_04.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2025/03/20 22:44:53 (UT+08:00) daisuke>
+# Time-stamp: <2025/03/24 19:30:51 (UT+08:00) daisuke>
 #
 
 # SQL query
-sqlite3 --header --column bsc5.db "select hr, name, ra_str, dec_str, \
-glon, glat, vmag from bsc where glat > 85.0 order by glat desc;"
+sqlite3 --header --column bsc5.db \
+	"select hr, name, ra_str, dec_str, glon, glat, vmag from bsc \
+	where glat > 85.0 order by glat desc;"
