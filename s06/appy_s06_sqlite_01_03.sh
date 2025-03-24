29f243c7be73ded8d22925dc80ad14a67d38088e
Daisuke <mhdf23888@gmail.com>
2025-03-24 19:38:58 +0800

    adding "s06/appy_s06_sqlite_01_03.sh" on 24/Mar/2025

--- s06/appy_s06_sqlite_01_03.sh
+++ s06/appy_s06_sqlite_01_03.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2025/03/20 21:16:55 (UT+08:00) daisuke>
+# Time-stamp: <2025/03/24 19:28:52 (UT+08:00) daisuke>
 #
 
 # SQL query
-sqlite3 --header --column element.db "select Symbol, Name, AtomicMass, \
-StandardState, MeltingPoint, BoilingPoint from element \
-where StandardState is 'Liquid';"
+sqlite3 --header --column element.db \
+	"select Symbol, Name, AtomicMass, StandardState, \
+	MeltingPoint, BoilingPoint from element \
+	where StandardState is 'Liquid';"
