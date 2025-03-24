6ca71b58c6b0752a38c72f29c2e3f9510bb1c0fd
Daisuke <mhdf23888@gmail.com>
2025-03-24 19:38:19 +0800

    adding "s06/appy_s06_sqlite_00_17.sh" on 24/Mar/2025

--- s06/appy_s06_sqlite_00_17.sh
+++ s06/appy_s06_sqlite_00_17.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2025/03/20 20:31:10 (UT+08:00) daisuke>
+# Time-stamp: <2025/03/24 19:20:22 (UT+08:00) daisuke>
 #
 
-sqlite3 -header -column planet0.db "select name,mass,diameter,satellite, \
-ring,magnetic_field from planet;"
+# SQL query
+sqlite3 -header -column planet0.db \
+	"select name,mass,diameter,satellite, ring,magnetic_field from planet;"
