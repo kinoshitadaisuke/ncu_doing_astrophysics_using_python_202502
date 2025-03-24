bf31755e937ab6447ac688f00025a900c4787f05
Daisuke <mhdf23888@gmail.com>
2025-03-24 19:37:52 +0800

    adding "s06/appy_s06_sqlite_00_01.sh" on 24/Mar/2025

--- s06/appy_s06_sqlite_00_01.sh
+++ s06/appy_s06_sqlite_00_01.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2025/03/20 20:09:04 (UT+08:00) daisuke>
+# Time-stamp: <2025/03/24 19:16:13 (UT+08:00) daisuke>
 #
 
+# printing tables in database
 sqlite3 planet0.db ".tables"
