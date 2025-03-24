6dd1201f29b81ec5f3d6735981f43ea9699e2772
Daisuke <mhdf23888@gmail.com>
2025-03-24 19:38:11 +0800

    adding "s06/appy_s06_sqlite_00_12.sh" on 24/Mar/2025

--- s06/appy_s06_sqlite_00_12.sh
+++ s06/appy_s06_sqlite_00_12.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2025/03/20 20:23:48 (UT+08:00) daisuke>
+# Time-stamp: <2025/03/24 19:36:29 (UT+08:00) daisuke>
 #
 
-sqlite3 planet0.db "insert into planet values ('Neptune', 1.02E26, \
-4.9528E4, 16.1, 59800, -200, 14, 'Yes', 'Yes');"
+# adding data to table
+sqlite3 planet0.db \
+	"insert into planet values ('Neptune', 1.02E26, 4.9528E4, 16.1, \
+	59800, -200, 14, 'Yes', 'Yes');"
