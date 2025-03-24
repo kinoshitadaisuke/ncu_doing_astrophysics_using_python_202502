5549dba09ed85fc54bf0a4b1e738135584a0a859
Daisuke <mhdf23888@gmail.com>
2025-03-24 19:39:02 +0800

    adding "s06/appy_s06_sqlite_01_05.sh" on 24/Mar/2025

--- s06/appy_s06_sqlite_01_05.sh
+++ s06/appy_s06_sqlite_01_05.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2025/03/20 21:19:13 (UT+08:00) daisuke>
+# Time-stamp: <2025/03/24 19:29:50 (UT+08:00) daisuke>
 #
 
 # SQL query
-sqlite3 --header --column element.db "select Name, Symbol, \
-StandardState, MeltingPoint, BoilingPoint from element \
-where BoilingPoint < 300 order by BoilingPoint;"
+sqlite3 --header --column element.db \
+	"select Name, Symbol, StandardState, MeltingPoint, BoilingPoint \
+	from element where BoilingPoint < 300 order by BoilingPoint;"
