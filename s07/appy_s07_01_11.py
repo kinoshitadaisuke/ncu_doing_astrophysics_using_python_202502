8cb639e65537c5f9fd90fdf577afb2d3f52adaf2
Daisuke <mhdf23888@gmail.com>
2025-03-25 10:23:18 +0800

    adding "s07/appy_s07_01_11.py" on 25/Mar/2025

---
+++ s07/appy_s07_01_11.py
@@ -0,0 +1,22 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/10/21 13:48:48 (UT+8) daisuke>
+#
+
+# importing astropy module
+import astropy.units
+
+# units
+u_K        = astropy.units.K
+u_keV      = astropy.units.keV
+u_T_energy = astropy.units.temperature_energy ()
+
+# energy
+energy = 1.0 * u_keV
+
+# calculation of equivalent temperature for energy of 1 keV
+T = energy.to (u_K, equivalencies=u_T_energy)
+
+# printing result
+print (f'energy = {energy:g}  ==>  temperature = {T:g}')
