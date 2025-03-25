0557dd86aa39db4942f74d40305e6289fd4d4f9e
Daisuke <mhdf23888@gmail.com>
2025-03-25 10:23:44 +0800

    adding "s07/appy_s07_02_11.py" on 25/Mar/2025

---
+++ s07/appy_s07_02_11.py
@@ -0,0 +1,21 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/10/21 14:28:03 (UT+8) daisuke>
+#
+
+# importing astropy module
+import astropy.table
+
+# VOTable file
+file_vot = 'exoplanet.vot'
+
+# reading VOTable file and making an Astropy table
+table_exoplanet = astropy.table.Table.read (file_vot)
+
+# printing information of planet "51 Peg b"
+for i in range (len (table_exoplanet)):
+    if (table_exoplanet[i]["name"] == "51 Peg b"):
+        print (table_exoplanet[i]["name", "mass", "semi_major_axis", \
+                                  "orbital_period", "detection_type", \
+                                  "discovered"])
