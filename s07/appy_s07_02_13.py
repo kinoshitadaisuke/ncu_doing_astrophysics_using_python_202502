924d14151159acf4727cf515d4d8927a22fb2096
Daisuke <mhdf23888@gmail.com>
2025-03-25 10:23:47 +0800

    adding "s07/appy_s07_02_13.py" on 25/Mar/2025

---
+++ s07/appy_s07_02_13.py
@@ -0,0 +1,21 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/10/21 14:29:18 (UT+8) daisuke>
+#
+
+# importing astropy module
+import astropy.table
+
+# VOTable file
+file_vot = 'exoplanet.vot'
+
+# reading VOTable file and making an Astropy table
+table_exoplanet = astropy.table.Table.read (file_vot)
+
+# printing information of planet discovered in 1995, 1996, and 1997
+mask = (table_exoplanet["discovered"] >= 1995) \
+    & (table_exoplanet["discovered"] <= 1997)
+print (table_exoplanet[mask]["name", "mass", "semi_major_axis", \
+                             "orbital_period", "detection_type", \
+                             "discovered"])
