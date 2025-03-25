34c1cddc010f348403c63c201234f8be383116e8
Daisuke <mhdf23888@gmail.com>
2025-03-25 10:23:54 +0800

    adding "s07/appy_s07_03_02.py" on 25/Mar/2025

---
+++ s07/appy_s07_03_02.py
@@ -0,0 +1,22 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/10/21 14:37:04 (UT+8) daisuke>
+#
+
+# importing datetime module
+import datetime
+
+# importing astropy module
+import astropy.time
+
+# getting current date/time using datetime module
+now_datetime = datetime.datetime.now (tz=datetime.timezone.utc)
+
+# constructing Astropy's Time object from a string
+now = astropy.time.Time (now_datetime, scale='utc')
+
+# printing date/time
+print (f'now  = {now}')
+print (f'     = JD  {now.jd:14.6f}')
+print (f'     = MJD {now.mjd:14.6f}')
