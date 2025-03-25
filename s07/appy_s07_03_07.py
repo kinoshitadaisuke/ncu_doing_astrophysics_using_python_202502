0668b40defda48693795d73fac2e1853518de19b
Daisuke <mhdf23888@gmail.com>
2025-03-25 10:24:05 +0800

    adding "s07/appy_s07_03_07.py" on 25/Mar/2025

---
+++ s07/appy_s07_03_07.py
@@ -0,0 +1,22 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/10/21 14:41:49 (UT+8) daisuke>
+#
+
+# importing astropy module
+import astropy.time
+import astropy.units
+
+# hour
+u_hr = astropy.units.hr
+
+# time t1
+t1 = astropy.time.Time ('2024-12-31 18:00:00', format='iso', scale='utc')
+
+# calculation of t1 + 12-hr
+t2 = t1 + 12.0 * u_hr
+
+# printing date/time
+print (f't1              = {t1}')
+print (f't2 = t1 + 12-hr = {t2}')
