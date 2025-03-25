8fdf1d60770a6f51ee9b128d9705618c599bb90e
Daisuke <mhdf23888@gmail.com>
2025-03-25 10:24:02 +0800

    adding "s07/appy_s07_03_06.py" on 25/Mar/2025

---
+++ s07/appy_s07_03_06.py
@@ -0,0 +1,22 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/10/21 14:39:54 (UT+8) daisuke>
+#
+
+# importing astropy module
+import astropy.time
+
+# time t1
+t1 = astropy.time.Time ('2025-01-01 00:00:00', format='iso', scale='utc')
+
+# time t2
+t2 = astropy.time.Time ('2026-01-01 00:00:00', format='iso', scale='utc')
+
+# time between t1 and t2
+delta_t = t2 - t1
+
+# printing date/time
+print (f't1      = {t1}')
+print (f't2      = {t2}')
+print (f't2 - t1 = {delta_t} day = {delta_t.sec:g} sec')
