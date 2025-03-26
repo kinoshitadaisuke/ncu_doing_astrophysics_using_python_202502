a87a4d962e5151ef8305321a8b8e2b7715baaa21
Daisuke <mhdf23888@gmail.com>
2025-03-26 09:29:26 +0800

    adding "s07/appy_s07_02_00.py" on 26/Mar/2025

--- s07/appy_s07_02_00.py
+++ s07/appy_s07_02_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/10/21 13:56:10 (UT+8) daisuke>
+# Time-stamp: <2025/03/26 09:23:33 (UT+08:00) daisuke>
 #
 
 # importing astropy module
