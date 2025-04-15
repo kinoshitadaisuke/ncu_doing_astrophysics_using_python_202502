fb52ddc233adff0418132268b587a8bf7c973cb6
Daisuke <mhdf23888@gmail.com>
2025-04-16 03:55:46 +0800

    adding "s09/appy_s09_00_09.py" on 16/Apr/2025

---
+++ s09/appy_s09_00_09.py
@@ -0,0 +1,35 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/11/04 14:18:52 (UT+8) daisuke>
+#
+
+# importing astropy module
+import astropy.time
+
+# importing astroquery module
+import astroquery.jplhorizons
+
+# date/time
+date = astropy.time.Time ('2025-01-01 00:00:00')
+
+# number of asteroids to get position
+n_asteroids = 10
+
+# printing header
+print (f"Positions of asteroids on {date}:")
+
+# processing for each asteroid
+for i in range (1, n_asteroids + 1):
+    # set-up a query for JPL Horizons
+    query = astroquery.jplhorizons.Horizons (id=f"{i}", \
+                                             id_type='smallbody', \
+                                             epochs=date.jd)
+
+    # fetching ephemeris of asteroid
+    eph = query.ephemerides ()
+
+    # priting RA and Dec of asteroid
+    print (f" {eph['targetname'][0]:32s}:" \
+           + f" (RA, Dec) = ({eph['RA'][0]:8.4f} deg," \
+           + f" {eph['DEC'][0]:+8.4f} deg)")
