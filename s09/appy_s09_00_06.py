ef6109429155a5f029e553131e43deea0c99fb18
Daisuke <mhdf23888@gmail.com>
2025-04-17 22:26:59 +0800

    adding "s09/appy_s09_00_06.py" on 17/Apr/2025

--- s09/appy_s09_00_06.py
+++ s09/appy_s09_00_06.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/11/06 08:05:20 (UT+8) daisuke>
+# Time-stamp: <2025/04/17 18:59:23 (UT+08:00) daisuke>
 #
 
 # importing gzip module
