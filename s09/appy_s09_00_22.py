01f58704fcb86f31c9b403a297a18a53abbf4af9
Daisuke <mhdf23888@gmail.com>
2025-04-19 11:27:50 +0800

    adding "s09/appy_s09_00_22.py" on 19/Apr/2025

--- s09/appy_s09_00_22.py
+++ s09/appy_s09_00_22.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/11/06 08:13:47 (UT+8) daisuke>
+# Time-stamp: <2025/04/19 10:51:29 (UT+08:00) daisuke>
 #
 
 # importing numpy module
@@ -17,7 +17,7 @@ import matplotlib.figure
 import matplotlib.backends.backend_agg
 
 # date/time
-date = astropy.time.Time ('2025-01-01 00:00:00')
+date = astropy.time.Time ('2025-05-01 00:00:00')
 
 # input data file name
 file_input = 'asteroids_100000.data'
