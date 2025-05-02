bd8576daaba84ce9cb6a425515c8e11775cd4e5c
Daisuke <mhdf23888@gmail.com>
2025-05-02 08:37:22 +0800

    adding "s10/appy_s10_02_00.py" on  2/May/2025

---
+++ s10/appy_s10_02_00.py
@@ -0,0 +1,17 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/11/12 09:46:23 (UT+8) daisuke>
+#
+
+# importing git module
+import git
+
+# URL of repository
+url_repo = 'https://github.com/paulfitz/exoplanets.git'
+
+# directory name of downloaded repository
+dir_repo = 'exoplanets'
+
+# downloading repository
+repo = git.Repo.clone_from (url_repo, dir_repo)
