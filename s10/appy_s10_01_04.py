c3a04f2f54d69973d81936e4bf85957f7e95b403
Daisuke <mhdf23888@gmail.com>
2025-05-02 08:37:16 +0800

    adding "s10/appy_s10_01_04.py" on  2/May/2025

---
+++ s10/appy_s10_01_04.py
@@ -0,0 +1,18 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/11/12 09:31:51 (UT+8) daisuke>
+#
+
+# importing astropy module
+import astropy.io.ascii
+
+# CSV file name
+file_csv = 'honey-badger/examples/planets/planets.csv'
+
+# reading a CSV file and storing data in an astropy table
+table = astropy.io.ascii.read (file_csv, format='csv')
+
+# printing the information about Jupiter
+mask = (table['Planet'] == 'JUPITER')
+print (f'{table[mask]["Planet", "Number of Moons"]}')
