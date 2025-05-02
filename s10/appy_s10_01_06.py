c0ae984be4583c0c2ab5db48b5e36e71d0161eaa
Daisuke <mhdf23888@gmail.com>
2025-05-02 08:37:19 +0800

    adding "s10/appy_s10_01_06.py" on  2/May/2025

---
+++ s10/appy_s10_01_06.py
@@ -0,0 +1,17 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/11/12 09:37:59 (UT+8) daisuke>
+#
+
+# importing astropy module
+import astropy.io.ascii
+
+# CSV file name
+file_csv = 'hyg/hyg/v3/hyg_v38.csv'
+
+# reading a CSV file and storing data in an astropy table
+table = astropy.io.ascii.read (file_csv, format='csv')
+
+# printing astropy table
+print (table)
