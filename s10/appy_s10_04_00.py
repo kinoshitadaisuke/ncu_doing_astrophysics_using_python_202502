c1004e339be7fd440929f82bc67d5f00458a1c47
Daisuke <mhdf23888@gmail.com>
2025-05-04 13:43:56 +0800

    adding "s10/appy_s10_04_00.py" on  4/May/2025

--- s10/appy_s10_04_00.py
+++ s10/appy_s10_04_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/11/13 07:32:26 (UT+8) daisuke>
+# Time-stamp: <2025/05/04 11:58:42 (UT+08:00) daisuke>
 #
 
 # importing urllib module
