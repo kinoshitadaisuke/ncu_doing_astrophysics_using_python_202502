c1d3dcffe830f6e5a5b149e14dc0bb21db4fcb3d
Daisuke <mhdf23888@gmail.com>
2025-05-04 13:43:58 +0800

    adding "s10/appy_s10_04_01.py" on  4/May/2025

--- s10/appy_s10_04_01.py
+++ s10/appy_s10_04_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/11/13 07:34:52 (UT+8) daisuke>
+# Time-stamp: <2025/05/04 12:03:46 (UT+08:00) daisuke>
 #
 
 # original CSV file
