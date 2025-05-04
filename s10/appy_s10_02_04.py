43b333c7cf49e390c0a5a506a79bb441ff73f6ea
Daisuke <mhdf23888@gmail.com>
2025-05-04 13:43:38 +0800

    adding "s10/appy_s10_02_04.py" on  4/May/2025

--- s10/appy_s10_02_04.py
+++ s10/appy_s10_02_04.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/11/12 09:49:17 (UT+8) daisuke>
+# Time-stamp: <2025/05/04 11:40:13 (UT+08:00) daisuke>
 #
 
 # importing json module
