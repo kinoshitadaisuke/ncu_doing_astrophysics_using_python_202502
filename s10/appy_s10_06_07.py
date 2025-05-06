62d5ee24bdf4fadcdb83de60126e48a02947b437
Daisuke <mhdf23888@gmail.com>
2025-05-06 08:35:59 +0800

    adding "s10/appy_s10_06_07.py" on  6/May/2025

--- s10/appy_s10_06_07.py
+++ s10/appy_s10_06_07.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/11/13 08:20:53 (UT+8) daisuke>
+# Time-stamp: <2025/05/04 13:21:17 (UT+08:00) daisuke>
 #
 
 # importing git module
