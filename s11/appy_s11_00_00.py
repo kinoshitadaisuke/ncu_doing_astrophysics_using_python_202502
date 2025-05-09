33af6a600b2f77bb11b6ba374a1924544049ab01
Daisuke <mhdf23888@gmail.com>
2025-05-09 11:45:31 +0800

    adding "s11/appy_s11_00_00.py" on  9/May/2025

--- s11/appy_s11_00_00.py
+++ s11/appy_s11_00_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/11/20 11:58:27 (UT+8) daisuke>
+# Time-stamp: <2025/05/09 11:43:37 (UT+08:00) daisuke>
 #
 
 # check of availability of rebound module
