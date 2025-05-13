17f43bee9b2ed4cf6897706ebff8051f9f77b235
Daisuke <mhdf23888@gmail.com>
2025-05-13 10:15:48 +0800

    adding "s11/appy_s11_05_06.py" on 13/May/2025

--- s11/appy_s11_05_06.py
+++ s11/appy_s11_05_06.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/11/21 21:34:16 (UT+8) daisuke>
+# Time-stamp: <2025/05/13 09:50:19 (UT+08:00) daisuke>
 #
 
 # importing argparse module
