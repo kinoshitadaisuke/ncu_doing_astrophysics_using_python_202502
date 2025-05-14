d45b1f42d20c53681a3b5a1686440f68c0f938c5
Daisuke <mhdf23888@gmail.com>
2025-05-14 09:04:05 +0800

    adding "s12/appy_s12_03_01.py" on 14/May/2025

---
+++ s12/appy_s12_03_01.py
@@ -0,0 +1,19 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/12/04 16:29:36 (UT+8) daisuke>
+#
+
+# importing tarfile module
+import tarfile
+
+# gzipped tar file
+file_targz = 'linear.tar.gz'
+
+# target directory for extraction of files
+dir_target = 'linear'
+
+# opening tar file
+with tarfile.open (file_targz, 'r:gz') as fh_targz:
+    # extracting files
+    fh_targz.extractall (path=dir_target)
