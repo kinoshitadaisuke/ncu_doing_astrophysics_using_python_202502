ff1bd9c168b14162a8d4bade6621d214b10bfbae
Daisuke <mhdf23888@gmail.com>
2025-05-20 10:21:49 +0800

    adding "s12/appy_s12_02_10.py" on 20/May/2025

--- s12/appy_s12_02_10.py
+++ s12/appy_s12_02_10.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/12/06 10:44:50 (UT+8) daisuke>
+# Time-stamp: <2025/05/20 09:16:55 (UT+08:00) daisuke>
 #
 
 # importing numpy module
