1bfc4be8aac63b4489a565e3e18ea17a93101e0f
Daisuke <mhdf23888@gmail.com>
2025-05-26 22:25:01 +0800

    adding "s14/appy_s14_05_03.py" on 26/May/2025

---
+++ s14/appy_s14_05_03.py
@@ -0,0 +1,25 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/12/22 20:06:59 (UT+8) daisuke>
+#
+
+# importing subprocess module
+import subprocess
+
+# ffmpeg command
+ffmpeg = 'ffmpeg'
+
+# ffmpeg options
+options_ffmpeg = f'-f image2 -start_number 0 -framerate 30' \
+    + f' -i comets/comets_%04d.png' \
+    + f' -an -vcodec libx264 -pix_fmt yuv420p -threads 4'
+
+# output file name
+file_output = 'comets.mp4'
+
+# command to be executed
+command_ffmpeg = f'{ffmpeg} {options_ffmpeg} {file_output}'
+
+# execution of command
+subprocess.run (command_ffmpeg, shell=True)
