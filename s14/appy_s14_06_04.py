c607d95cdc0094004464bfed1b610b7e38d61102
Daisuke <mhdf23888@gmail.com>
2025-05-29 19:53:58 +0800

    adding "s14/appy_s14_06_04.py" on 29/May/2025

--- s14/appy_s14_06_04.py
+++ s14/appy_s14_06_04.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/12/22 20:28:22 (UT+8) daisuke>
+# Time-stamp: <2025/05/29 19:52:34 (UT+08:00) daisuke>
 #
 
 # importing datetime module
