8c189872cbc50a71113a67501a31e3b9a9fb5a36
Daisuke <mhdf23888@gmail.com>
2025-06-02 09:57:14 +0800

    adding "s14/appy_s14_07_01.py" on  2/Jun/2025

--- s14/appy_s14_07_01.py
+++ s14/appy_s14_07_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/12/25 09:44:50 (UT+8) daisuke>
+# Time-stamp: <2025/05/31 20:34:39 (UT+08:00) daisuke>
 #
 
 # importing gzip module
