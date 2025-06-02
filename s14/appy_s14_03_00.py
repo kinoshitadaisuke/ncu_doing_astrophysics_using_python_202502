71a884b9f609999c8412013ad2be42bd2f907274
Daisuke <mhdf23888@gmail.com>
2025-06-02 09:56:51 +0800

    adding "s14/appy_s14_03_00.py" on  2/Jun/2025

--- s14/appy_s14_03_00.py
+++ s14/appy_s14_03_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2024/12/22 17:53:19 (UT+8) daisuke>
+# Time-stamp: <2025/05/29 12:24:47 (UT+08:00) daisuke>
 #
 
 # importing rebound module
