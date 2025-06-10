5e174aa32bbf3d7e9a698d8fb6ff785c91f1bda5
Daisuke <mhdf23888@gmail.com>
2025-06-10 12:33:26 +0800

    adding "s15/appy_s15_04_01.py" on 10/Jun/2025

--- s15/appy_s15_04_01.py
+++ s15/appy_s15_04_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2025/01/04 09:42:31 (UT+8) daisuke>
+# Time-stamp: <2025/06/10 11:03:40 (UT+08:00) daisuke>
 #
 
 # importing numpy module
