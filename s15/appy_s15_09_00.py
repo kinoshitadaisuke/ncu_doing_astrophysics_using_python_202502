3cc130f43408342409c32f54f1372af1710acab5
Daisuke <mhdf23888@gmail.com>
2025-06-10 12:34:42 +0800

    adding "s15/appy_s15_09_00.py" on 10/Jun/2025

--- s15/appy_s15_09_00.py
+++ s15/appy_s15_09_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2025/01/05 10:48:46 (UT+8) daisuke>
+# Time-stamp: <2025/06/10 12:06:14 (UT+08:00) daisuke>
 #
 
 # importing argparse
