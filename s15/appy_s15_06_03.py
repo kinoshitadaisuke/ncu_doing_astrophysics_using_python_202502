6a7e67534c4fef20f685199eb32a432f930f7f95
Daisuke <mhdf23888@gmail.com>
2025-06-10 12:33:59 +0800

    adding "s15/appy_s15_06_03.py" on 10/Jun/2025

--- s15/appy_s15_06_03.py
+++ s15/appy_s15_06_03.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2025/01/04 19:09:33 (UT+8) daisuke>
+# Time-stamp: <2025/06/10 11:34:11 (UT+08:00) daisuke>
 #
 
 # data file
